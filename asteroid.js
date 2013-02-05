e98390209ad2149c71138ddf15c945b8f09cf179
Thomas Sarlandie <thomas@sarlandie.net>
2013-02-05 18:52:37 +0000

    Mostly static content for the homepage

--- asteroid.js
+++ asteroid.js
@@ -1,13 +1,31 @@
 if (Meteor.isClient) {
+	Meteor.startup(function() {
+		Session.set("clicks", 0);
+	});
+	
   Template.hello.greeting = function () {
-    return "Welcome to asteroid.";
+    return "Welcome to asteroid!";
   };
+
+	Template.hello.clicks = function() {
+		if (Session.get("clicks") == 0) { 
+			return "You should really click on that button! ";
+		}
+		else {
+			return "You have clicked: " + Session.get("clicks") + " times.";
+		}
+	};
+
+	Template.connectionStatus.status = function() {
+		return "Connection status: " + Meteor.status().status;
+	};
 
   Template.hello.events({
     'click input' : function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+        console.log("You pressed the button - clicks=" + Session.get("clicks"));
+			Session.set("clicks", Session.get("clicks")+1);
     }
   });
 }
