a58c98c912d267ad7c383a04358be23229f4df64
Thomas Sarlandie <thomas@sarlandie.net>
2013-02-17 18:55:40 +0000

    Adding blank gettingstarted page and a link in the nav to get to it

--- client/body.js
+++ client/body.js
@@ -1,3 +1,10 @@
 Template.page.created = function() {
   analytics.init();
 }
+
+Template.page.gettingstarted = function() {
+  console.log("Session.get('gettingstarted'): %s", 
+    Session.get("gettingstarted"));
+  if (Session.get("gettingstarted")) return true;
+  return false;
+}
