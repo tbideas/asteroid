261a93e0fe6b1fc5346f9404cd72d7a33fca4aeb
Thomas Sarlandie <thomas@sarlandie.net>
2013-03-20 11:57:30 +0000

    Added a very simple routing system

--- client/body.js
+++ client/body.js
@@ -1,3 +1,23 @@
 Template.page.created = function() {
   analytics.init();
 }
+
+/* A very simple router */
+Template.page.content = function() {
+	if (Meteor.user()) {
+		switch (Session.get("page")) {
+			case "home":
+        return Template.home();
+        break;
+			case "dashboard":
+				return Template.dashboard();
+        break;
+			default:
+				return Template.dashboard();
+        break;
+		}
+	}
+	else {
+		return Template.home();
+	}
+}
