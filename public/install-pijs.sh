fb89b19d9a1d1357d2926b8a1f30c7fc8fdb8b68
Thomas Sarlandie <thomas@sarlandie.net>
2013-03-21 11:58:40 +0000

    Fix link to ping server

--- public/install-pijs.sh
+++ public/install-pijs.sh
@@ -8,7 +8,7 @@ PING_INSTALL_2='http://io.tbideas.com/pijs/install-done'
 echo "# Installing piJS on this Raspberry Pi..."
 
 echo "## Pinging stat server to record a new installation... (no personal info sent)"
-curl PING_INSTALL_1
+curl $PING_INSTALL_1
 
 echo "## Downloading nodejs"
 curl $NODE_JS -o /tmp/nodejs.tar.gz
@@ -25,4 +25,4 @@ echo "## Adding automatic startup script (/etc/init.d/pijs)"
 cp /usr/local/node
 
 echo "# Installation finished! Pinging stat server"
-curl PING_INSTALL_2
+curl $PING_INSTALL_2
