adcb1094fd11543a9de860310bf2b586a5c1bcea
Thomas Sarlandie <thomas@sarlandie.net>
2013-05-04 12:46:54 +0000

    Improving emails
     * Use a dedicated mailgun account
     * added the email package

--- server/main.js
+++ server/main.js
@@ -1,6 +1,14 @@
 Meteor.startup(function () {
-  if (Meteor.settings.emails && Meteor.settings.emails.from)
+  if (Meteor.settings.emails && Meteor.settings.emails.mailURL) {
+    process.env.MAIL_URL = Meteor.settings.emails.mailURL;
+  }
+  else {
+    console.error("No Settings.emails.mailURL set!");
+  }
+
+  if (Meteor.settings.emails && Meteor.settings.emails.from) {
     Accounts.emailTemplates.from = Meteor.settings.emails.from;
+  }
 
   Accounts.emailTemplates.siteName = "pijs.io";
 });
