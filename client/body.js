b80d58c60be2654732c59f618fd3b76a962393b6
Thomas Sarlandie <thomas@sarlandie.net>
2013-05-09 00:31:52 +0000

    Adding uservoice for support/feedback

--- client/body.js
+++ client/body.js
@@ -83,3 +83,17 @@ Template.navLink.events({
       router.gotoPage(template.data.path);
   }
 });
+Template.userVoiceItem.events({
+  'click': function(event, template) {
+    event.preventDefault();
+
+    UserVoice.push(['showLightbox', 'classic_widget', {
+      mode: 'full',
+      primary_color: '#cc6d00',
+      link_color: '#007dbf',
+      default_mode: 'support',
+      forum_id: 205991
+    }]);
+
+  }
+});
---
+++ client/js/uservoice.js
@@ -0,0 +1,6 @@
+<!-- UserVoice JavaScript SDK (only needed once on a page) -->
+(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/EedjgzxYFihpzb0suSqIzg.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()
+
+<!-- A function to launch the Classic Widget -->
+
+UserVoice = window.UserVoice || [];
