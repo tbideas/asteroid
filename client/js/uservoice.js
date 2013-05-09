f60d03ea0622bb2e855687c67860a8a8a18a0c02
Thomas Sarlandie <thomas@sarlandie.net>
2013-05-09 00:35:23 +0000

    remove html comment in uservoice js file

--- client/js/uservoice.js
+++ client/js/uservoice.js
@@ -1,6 +1,3 @@
-<!-- UserVoice JavaScript SDK (only needed once on a page) -->
-(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/EedjgzxYFihpzb0suSqIzg.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()
-
-<!-- A function to launch the Classic Widget -->
+(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/EedjgzxYFihpzb0suSqIzg.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})();
 
 UserVoice = window.UserVoice || [];
